c82e8c5ff8b9b9c369eaa1468b15048b66bfa319
Pranay Sarkar <pranaysarkar22@gmail.com>
2022-02-21 23:57:12 +0100

    na: test cases fixed [Pranay Sarkar] [V]

--- eth-contracts/contracts/SolnSquareVerifier.sol
+++ eth-contracts/contracts/SolnSquareVerifier.sol
@@ -1,59 +1,111 @@
 pragma solidity >=0.4.21 <0.6.0;
+pragma experimental ABIEncoderV2;
 
 // TODO define a contract call to the zokrates generated solidity contract <Verifier> or <renamedVerifier>
-
-
+import "./ERC721Mintable.sol";
+import "./verifier.sol";
 
 // TODO define another contract named SolnSquareVerifier that inherits from your ERC721Mintable class
+contract SolnSquareVerifier is Verifier, ERC721Mintable {
+    // TODO define a solutions struct that can hold an index & an address
+    struct Solution {
+        address to;
+        uint256 tokenId;
+        bytes32 keyId;
+        bool exists;
+        uint256 minted;
+    }
 
+    uint256 private solutionCount;
+    // TODO define an array of the above struct
+    mapping(uint256 => Solution) private _solutions;
+    // TODO define a mapping to store unique solutions submitted
+    mapping(bytes32 => uint256) private key2token;
 
+    modifier SolutionExists(uint256 tokenId) {
+        require(_solutions[tokenId].exists == true, "No Solution provided");
+        _;
+    }
+    modifier AddressCorrect(address to, uint256 tokenId) {
+        require(_solutions[tokenId].to == to, "invalid Address provided");
+        _;
+    }
 
-// TODO define a solutions struct that can hold an index & an address
+    // TODO Create an event to emit when a solution is added
+    event SolutionAdded(bytes32 generatedId, uint256 tokenId, address owner);
 
+    // TODO Create a function to add the solutions to the array and emit the event
+    function addSolution(
+        address to,
+        uint256 tokenId,
+        uint256[2] memory a,
+        uint256[2][2] memory b,
+        uint256[2] memory c,
+        uint256[2] memory input
+    ) public returns (bytes32) {
+        Verifier.Proof memory proof = Verifier.Proof(
+            Pairing.G1Point(a[0], a[1]),
+            Pairing.G2Point(b[0], b[1]),
+            Pairing.G1Point(c[0], c[1])
+        );
+        bool validProof = verifyTx(proof, input);
+        require(validProof == true, "Proof is not valid");
+        bytes32 keyId = keccak256(
+            abi.encodePacked(
+                to,
+                tokenId,
+                a[0],
+                a[1],
+                b[0],
+                b[1],
+                c[0],
+                c[1],
+                input
+            )
+        );
 
-// TODO define an array of the above struct
+        require(_solutions[tokenId].exists == false, "Solution exists");
+        key2token[keyId] = tokenId;
+        _solutions[tokenId] = Solution(to, tokenId, keyId, true, 0);
+        solutionCount = solutionCount + 1;
+        emit SolutionAdded(keyId, tokenId, to);
+        return keyId;
+    }
 
+    function tokenSolution(bytes32 keyId) external view returns (uint256) {
+        return key2token[keyId];
+    }
 
-// TODO define a mapping to store unique solutions submitted
+    function SolutionKeyId(address to, uint256 tokenId)
+        external
+        view
+        SolutionExists(tokenId)
+        AddressCorrect(to, tokenId)
+        returns (bytes32)
+    {
+        return _solutions[tokenId].keyId;
+    }
 
+    // TODO Create a function to mint new NFT only after the solution has been verified
+    //  - make sure the solution is unique (has not been used before)
+    //  - make sure you handle metadata as well as tokenSuplly
+    function mint(
+        address to,
+        uint256 tokenId,
+        bytes32 keyId
+    )
+        public
+        SolutionExists(tokenId)
+        AddressCorrect(to, tokenId)
+        returns (uint256)
+    {
+        require(
+            tokenId == key2token[keyId],
+            "KeyId doesn't match with tokenId"
+        );
 
-
-// TODO Create an event to emit when a solution is added
-
-
-
-// TODO Create a function to add the solutions to the array and emit the event
-
-
-
-// TODO Create a function to mint new NFT only after the solution has been verified
-//  - make sure the solution is unique (has not been used before)
-//  - make sure you handle metadata as well as tokenSuplly
-
-  
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        _solutions[tokenId].minted = _solutions[tokenId].minted++;
+        super.mint(to, tokenId);
+        return key2token[keyId];
+    }
+}
--- eth-contracts/test/TestERC721Mintable.js
+++ eth-contracts/test/TestERC721Mintable.js
@@ -1,4 +1,4 @@
-var ERC721MintableComplete = artifacts.require('ERC721MintableComplete');
+var ERC721Mintable = artifacts.require('ERC721Mintable');
 
 contract('TestERC721Mintable', accounts => {
 
@@ -7,7 +7,7 @@ contract('TestERC721Mintable', accounts => {
 
     describe('match erc721 spec', function () {
         beforeEach(async function () { 
-            this.contract = await ERC721MintableComplete.new({from: account_one});
+            this.contract = await ERC721Mintable.new("Udacity Property Coin Pranay", "UPCP",{from: account_one} );
 
             // TODO: mint multiple tokens
             for( i = 1;i<=10;i++) {
